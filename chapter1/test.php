0e966f9d3f531c799757dd07555b39fccaf0939e
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-18 19:53:52 +0530

    Form Handling With PHP

---
+++ chapter1/test.php
@@ -0,0 +1,10 @@
+<?php
+
+// PHP allows server to dynamically generate HTML before html is delivered to page.
+// PHP scripts are stored in web servers where they are processed and then send to browsers as HTML pages.
+// PHP script contains both HTML and PHP script which determines how html is generated dynamically.
+// PHP Script stores and retrives data from database and incorprates data into html file and generate dynamic ddta.
+// php scripts run on the server.
+// Every php script turned into HTML and CSS once it's stop running into server.
+// When form data got submitted ihat data goes to php script on server than php script process that data, after processing that data server returns a html file to client browser.
+// The action attribute on the form element is what causes the php script to run on the server when form got submitted.
